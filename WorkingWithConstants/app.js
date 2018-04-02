ff387dbaccf9a3fc3bbeddb471ff6b89d53b6add
Ramesh <ramesh@rsageventures.com>
2018-04-02 16:03:02 +0530

    GetFullName fn added

--- WorkingWithConstants/app.js
+++ WorkingWithConstants/app.js
@@ -1,5 +1,27 @@
-var app = angular.module('app', []);
+var app = angular.module("app", []);
 
-app.controller('controller1',['$scope', function($scope) {
-    $scope.name = 'Ramesh';
-}]);
+app.controller("controller1", [
+    "$scope",
+    "NameService", "$log",
+    function($scope, NameService, $log) {
+        $scope.name = "Ramesh";
+        $scope.fullName = "";
+        $scope.getFullName = function() {
+            $log.info('getFullName()');
+            $scope.fullName = NameService.getFullName($scope.name);
+        };
+    }
+]);
+
+app.constant("LAST_NAME", "Panthangi");
+
+app.service("NameService", [
+    "LAST_NAME",
+    function(LAST_NAME) {
+        this.getFullName = getFullName;
+        
+        function getFullName(name) {
+            return name + " " + LAST_NAME;
+        }
+    }
+]);
